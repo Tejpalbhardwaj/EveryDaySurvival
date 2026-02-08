ad58b8f9f95d9b67831fb5205422e3df456655c1
Vasant Rawat <basantrawat9268@gmail.com>
2026-02-09 01:25:24 +0530

    feat:Added fonnts color scheme Created CategoriesScreen

--- app/src/main/java/com/example/everydaysurvival/ui/theme/Color.kt
+++ app/src/main/java/com/example/everydaysurvival/ui/theme/Color.kt
@@ -8,4 +8,6 @@ val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
+val bgColor = Color(0xFF333338)
+val cardColor = Color(0xFFFACC6B)
 val Pink40 = Color(0xFF7D5260)
